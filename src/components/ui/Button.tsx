c55dfef2fdb021e3aa5405e145362149f77bbd42
anjeetsingh7155 <anjeetsingh7155>
2026-02-11 23:43:24 +0530

    improved the button code

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -12,33 +12,21 @@ onClick?: () => void;
 export const Button = (prop: ButtonProps) => {
 
 // Variant styles
-const variantClass = () => {
-switch (prop.variant) {
-case "primary":
-return "bg-blue-900 text-white hover:bg-blue-600";
-case "secondary":
-return "bg-indigo-100 text-indigo-700 hover:bg-indigo-300";
-default:
-return "";
+const variantClass =  {
+"primary":"bg-blue-900 text-white hover:bg-blue-600",
+"secondary":"bg-indigo-100 text-indigo-700 hover:bg-indigo-300"
 }
-};
 
 // Size styles
-const sizeClass = () => {
-switch (prop.size) {
-case "sm":
-return "px-3 py-1 text-sm";
-case "md":
-return "px-4 py-2 text-base";
-case "lg":
-return "px-6 py-3 text-lg";
-default:
-return "";
+const sizeClass = {
+ "sm":"px-3 py-1 text-sm",
+ "md":"px-4 py-2 text-base",
+"lg": "px-6 py-3 text-lg",
 }
-};
+
 
 return (
-<button onClick={prop.onClick} className={`flex items-center gap-2 rounded-md transition ${variantClass()} ${sizeClass()}`}>
+<button onClick={prop.onClick} className={`flex items-center gap-2 rounded-md transition ${variantClass[prop.variant]} ${sizeClass[prop.size]}`}>
 {prop.startIcon && <span>{prop.startIcon}</span>}
 {prop.text && <span>{prop.text}</span>} 
 {prop.endIcon && <span>{prop.endIcon}</span>}
