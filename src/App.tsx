9c66203a906a0a1566ffdb7e747853124bd80610
anjeetsingh7155 <anjeetsingh7155>
2026-03-06 15:39:20 +0530

    Made the static card component

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,15 @@
 import './App.css'
-import { Button } from './components/ui/Button'
-import { useState } from 'react'
-import { PlusIcon } from './components/Icons/PlusIcon'
+// import { Button } from './components/ui/Button'
+// import { useState } from 'react'
+// import { PlusIcon } from './components/Icons/PlusIcon'
+import { Card } from './components/ui/Card'
 
 function App() {
 
- const [count ,setCount] = useState(0)
-
   return (
-    <>
-    <div className='flex justify-center items-center flex-col h-screen gap-2'>
-       <h1 className="text-3xl font-bold  text-center ">
-        {count}
-      </h1>
-      <div className='flex justify-center gap-2'>
-        <Button onClick={()=>{setCount(count+1)}} variant='primary' size='md' text='Count' startIcon={<PlusIcon size='md'/>}/>
-      </div>
+    <div  className='p-6'>
+     <Card/> 
     </div>
-    </>
   )
 }
 
