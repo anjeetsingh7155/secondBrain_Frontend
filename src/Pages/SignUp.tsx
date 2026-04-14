5133e51f2553c95da2ef0e14b755726e5aebfd99
anjeetsingh7155 <anjeetsingh7155>
2026-04-14 15:36:08 +0530

    Completed the signup Page

--- src/Pages/SignUp.tsx
+++ src/Pages/SignUp.tsx
@@ -1,28 +1,51 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import { PlusIcon } from "../components/Icons/PlusIcon";
 import { Button } from "../components/ui/Button";
 import { Input } from "../components/ui/Input";
 import axios from "axios";
 import { Backend_Url } from "../config";
+import { useNavigate } from "react-router-dom";
 
 export const SignUp = () => {
-  const UsernameRef = useRef<HTMLInputElement>(null);
+    const UsernameRef = useRef<HTMLInputElement>(null);
   const EmailRef = useRef<HTMLInputElement>(null);
   const PasswordRef = useRef<HTMLInputElement>(null);
 
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
+
   async function signup() {
-    const Username = UsernameRef.current?.value;
-    const Email = EmailRef.current?.value;
-    const Password = PasswordRef.current?.value;
-    await axios.post(`${Backend_Url}/api/v1/signup`, {
+    try {
+      setError("");
+
+      const Username = UsernameRef.current?.value;
+      const Email = EmailRef.current?.value;
+      const Password = PasswordRef.current?.value;
+
+      const response = await axios.post(`${Backend_Url}/api/v1/signup`, {
         userName: Username,
         email: Email,
         password: Password,
-    });
-    alert('You have Signed Up')
-  }
+      });
 
-  return (
+      alert(response.data.message);
+      navigate("/signin");
+
+    } catch (err: unknown) {
+  if (axios.isAxiosError(err)) {
+    if (err.response?.status === 409) {
+      setError("This person already exists, please login");
+    } else if (err.response?.status === 403) {
+      setError("Invalid input data");
+    } else {
+      setError("Something went wrong");
+    }
+  } else {
+    setError("Unexpected error occurred");
+  }
+}
+  }
+    return (
     <div className="h-screen w-screen bg-linear-to-br from-gray-100 to-gray-300 flex justify-center items-center">
       <div className="bg-white shadow-2xl rounded-2xl p-8 w-87.5">
         
@@ -32,6 +55,13 @@ export const SignUp = () => {
             Join us and start your journey 🚀
           </p>
         </div>
+
+       
+        {error && (
+          <p className="text-red-500 text-sm text-center mb-2">
+            {error}
+          </p>
+        )}
 
         <div className="flex flex-col gap-4">
           <Input ref={UsernameRef} placeholder="Username" />
@@ -45,16 +75,20 @@ export const SignUp = () => {
             text="Sign Up"
             size="md"
             startIcon={<PlusIcon size="md" />}
-            onClick={signup}  
+            onClick={signup}
           />
         </div>
 
         <p className="text-center text-sm text-gray-500 mt-4">
           Already have an account?{" "}
-          <span className="text-indigo-600 cursor-pointer hover:underline">
+          <span
+            onClick={() => navigate("/signin")}
+            className="text-indigo-600 cursor-pointer hover:underline"
+          >
             Login
           </span>
         </p>
+
       </div>
     </div>
   );
